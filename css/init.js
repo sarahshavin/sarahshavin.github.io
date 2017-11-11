0f060663da29b98e895feaf811010acabf0fda31
sarahshavin <sarahshavin@gmail.com>
2017-11-11 16:24:13 -0500

    Adding photos and page changes

--- css/init.js
+++ css/init.js
@@ -5,4 +5,26 @@
     $('.parallax').parallax();
 
   }); // end of document ready
+
+
+  var slideindex = 1;
+  showDivs(slideIndex);
+
+  function plusdivs(n){
+    showdivs(slideindex += n);
+  }
+
+  function plusdivs(n){
+    var i;
+    var x =
+
+  document.getelementsbyclassname ("myslides");
+  if (n>x.lenth) {slideindex=1}
+  if (n<1) {slideindex = x.length};
+  for (i=0; i<x.length; i++) {
+      x[i].style.display = "none";
+  }
+    x[slideindex-1].style.display="block";
+  }
+  
 })(jQuery); // end of jQuery name space
